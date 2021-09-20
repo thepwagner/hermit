6620ed0f765adfe65ed01f8e70abc13cf91a337d
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-09-20 22:23:40 +0000

    download dependency script

---
+++ download.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -e
+
+DEST="${1:-/usr/local/bin}"
+
+CONTAINERD_VERSION="v1.5.5"
+CONTAINERD_SHASUM="8efc527ffb772a82021800f0151374a3113ed2439922497ff08f2596a70f10f1"
+
+RUNC_VERSION="v1.0.2"
+RUNC_SHASUM="44d1ba01a286aaf0b31b4be9c6abc20deab0653d44ecb0d93b4d0d20eac3e0b6"
+
+BUILDKIT_VERSION="v0.9.0"
+BUILDKIT_SHASUM="1b307268735c8f8e68b55781a6f4c03af38acc1bc29ba39ebaec6d422bccfb25"
+
+fetch() {
+  local url="$1"
+  local path="$2"
+  local sha="$3"
+
+  echo "$sha  $path" | sha256sum -c - && return 0
+  curl -Lo "$path" "$url"
+  echo "$sha  $path" | sha256sum -c -
+}
+
+download_containerd() {
+  local CONTAINERD_FILE="/tmp/containerd.tgz"
+  fetch "https://github.com/containerd/containerd/releases/download/${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION:1}-linux-amd64.tar.gz" \
+    "$CONTAINERD_FILE" \
+    "$CONTAINERD_SHASUM"
+  tar -xzf "$CONTAINERD_FILE" -C "$DEST" --strip-components=1
+  cat <<EOF > "/etc/containerd.toml"
+[grpc]
+  address = "/run/containerd/containerd.sock"
+  uid = 0
+  gid = 4
+EOF
+  cat <<EOF >"/etc/systemd/system/containerd.service"
+[Unit]
+Description=containerd container runtime
+After=network.target local-fs.target
+
+[Service]
+ExecStart=/usr/local/bin/containerd -c /etc/containerd.toml
+Type=notify
+Delegate=yes
+KillMode=process
+Group=adm
+Restart=always
+RestartSec=5
+LimitNPROC=infinity
+LimitCORE=infinity
+LimitNOFILE=infinity
+TasksMax=infinity
+OOMScoreAdjust=-999
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+download_runc() {
+  fetch "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.amd64" \
+    "$DEST/runc" \
+    "$RUNC_SHASUM"
+  chmod +x "$DEST/runc"
+}
+
+download_buildkit() {
+  local BUILDKIT_FILE="/tmp/buildkit.tgz"
+  fetch "https://github.com/moby/buildkit/releases/download/${BUILDKIT_VERSION}/buildkit-${BUILDKIT_VERSION}.linux-amd64.tar.gz" \
+    "$BUILDKIT_FILE" \
+    "$BUILDKIT_SHASUM"
+  tar -xzf "$BUILDKIT_FILE" -C "$DEST" --strip-components=1
+  cat<<EOF >"/etc/systemd/system/buildkit.socket"
+[Unit]
+Description=BuildKit
+
+[Socket]
+ListenStream=%t/buildkit/buildkitd.sock
+SocketMode=0660
+SocketUser=root
+SocketGroup=adm
+
+[Install]
+WantedBy=sockets.target
+EOF
+  cat<<EOF >"/etc/systemd/system/buildkit.service"
+[Unit]
+Description=BuildKit
+Requires=buildkit.socket
+After=buildkit.socket
+
+[Service]
+ExecStart=/usr/local/bin/buildkitd --addr fd:// --oci-worker=false --containerd-worker=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+download_containerd
+download_runc
+download_buildkit
+
+systemctl daemon-reload
+systemctl restart containerd.service
+systemctl restart buildkit.service
+systemctl restart buildkit.socket
