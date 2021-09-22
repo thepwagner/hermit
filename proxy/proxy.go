8a266621f8bdc03dab461942d97f90020d0f2725
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-09-22 22:42:19 +0000

    proxy tests

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -3,10 +3,8 @@ package proxy
 import (
 	"crypto/ecdsa"
 	"crypto/tls"
-	"crypto/x509"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
@@ -14,55 +12,52 @@ import (
 	"github.com/go-logr/logr"
 )
 
+// Proxy acts as an HTTP proxy, and TLS interceptor.
+// HTTP CONNECT requests, as a client would issue for an HTTPS server, are intercepted by a self-signed CA.
 type Proxy struct {
-	log  logr.Logger
-	addr string
-	pk   *ecdsa.PrivateKey
+	handler http.Handler
+	log     logr.Logger
+	addr    string
+	pk      *ecdsa.PrivateKey
 
-	srv         *http.Server
-	url         *url.URL
-	certs       *CertIssuer
-	Snapshotter *Snapshotter
+	certs *CertIssuer
+	srv   *http.Server
+	url   *url.URL
 }
 
-func NewProxy(snap *Snapshotter, opts ...ProxyOpt) (*Proxy, error) {
-	// pk, err := PrivateKey()
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// b, _ := x509.MarshalECPrivateKey(pk)
-	// _ = ioutil.WriteFile("key.der", b, 0600)
-
-	b, err := ioutil.ReadFile("key.der")
-	if err != nil {
-		return nil, err
-	}
-	pk, err := x509.ParseECPrivateKey(b)
-	if err != nil {
-		return nil, err
-	}
-
+func NewProxy(handler http.Handler, opts ...ProxyOpt) (*Proxy, error) {
 	p := &Proxy{
-		log:         logr.Discard(),
-		addr:        "localhost:0",
-		Snapshotter: snap,
+		log:     logr.Discard(),
+		addr:    "localhost:0",
+		handler: handler,
 	}
 	for _, opt := range opts {
 		opt(p)
 	}
-	certs, err := NewCertIssuer(pk)
+
+	// Prepare the CA
+	if p.pk == nil {
+		pk, err := PrivateKey()
+		if err != nil {
+			return nil, err
+		}
+		p.pk = pk
+	}
+	certs, err := NewCertIssuer(p.pk)
 	if err != nil {
 		return nil, err
 	}
 	p.certs = certs
-	p.log.Info("certificate issuer", "key", fmt.Sprintf("%x", pk.PublicKey.X.Bytes()))
+	p.log.Info("certificate issuer", "key", fmt.Sprintf("%x", p.pk.PublicKey.X.Bytes()))
 
+	// Bind first, so we can read a randomly assigned port
 	l, err := net.Listen("tcp4", p.addr)
 	if err != nil {
 		return nil, fmt.Errorf("binding listener: %w", err)
 	}
 	p.log.Info("proxy listening", "addr", l.Addr().String())
 
+	// Embed and start a HTTP server:
 	p.srv = &http.Server{Handler: p}
 	p.url, _ = url.Parse(fmt.Sprintf("http://%s", l.Addr().(*net.TCPAddr).String()))
 	go func() {
@@ -97,26 +92,33 @@ func (p *Proxy) URL() *url.URL {
 	return p.url
 }
 
+func (p *Proxy) IssuerCertPEM() []byte {
+	return p.certs.CertPEM()
+}
+
 func (p *Proxy) Close() error {
 	p.log.Info("proxy closing")
 	return p.srv.Close()
 }
 
+const wellKnownCertPath = "/.well-known/hermit/proxy-cert"
+
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.log.Info("received request", "method", r.Method, "url", r.URL.String())
 
-	if r.URL.Path == "/.well-known/hermit/proxy-cert" {
-		w.Header().Add("Content-Type", "application/x-pem-file")
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(p.certs.CertPEM())
+	// Handle the internal route to provide the proxy's certificate
+	if r.URL.Path == wellKnownCertPath {
+		p.serveIssuerCert(w)
 		return
 	}
 
+	// HTTP requests are passed directly to handler:
 	if r.Method != http.MethodConnect {
-		p.Snapshotter.ServeHTTP(w, r)
+		p.handler.ServeHTTP(w, r)
 		return
 	}
 
+	// Issue a certificate for the requested host:
 	host, port, err := net.SplitHostPort(r.Host)
 	if err != nil || port != "443" {
 		http.Error(w, "bad host", http.StatusServiceUnavailable)
@@ -130,6 +132,7 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 
+	// Pretend to CONNECT, using our certificate. "hey it's me ur brother"
 	conn, _, err := w.(http.Hijacker).Hijack()
 	if err != nil {
 		p.log.Error(err, "proxy issue cert")
@@ -137,7 +140,6 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
-
 	tlsConn := tls.Server(conn, &tls.Config{
 		Certificates: []tls.Certificate{*cert},
 	})
@@ -147,17 +149,51 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer tlsConn.Close()
 
-	wrapped, sig := p.signallingHandler(p.Snapshotter, host)
+	// Serve requests on the intercepted connection using our handler:
+	wrapped, sig := p.signallingHandler(p.handler, host)
 	http.Serve(newConnListener(tlsConn), wrapped)
 	<-sig
 	return
 }
 
+func (p *Proxy) serveIssuerCert(w http.ResponseWriter) {
+	w.Header().Add("Content-Type", "application/x-pem-file")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(p.IssuerCertPEM())
+}
+
+// signallingHandler wraps the given handler, and returns a channel that is closed when the handler closes the connection.
+func (p *Proxy) signallingHandler(h http.Handler, host string) (http.Handler, <-chan struct{}) {
+	sig := make(chan struct{})
+	return &chanHandlerWrapper{h: h, signal: sig, log: p.log, host: host}, sig
+}
+
+type chanHandlerWrapper struct {
+	h      http.Handler
+	host   string
+	signal chan struct{}
+	log    logr.Logger
+}
+
+func (c *chanHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.URL.Scheme = "https"
+	r.URL.Host = c.host
+	r.Host = c.host
+	c.log.Info("intercepted request", "method", r.Method, "url", r.URL.String())
+	c.h.ServeHTTP(w, r)
+	if w.Header().Get("Connection") == "close" {
+		close(c.signal)
+	}
+}
+
+// connListener is Accept()s connections from a channel instead of binding a port.
 type connListener struct {
 	conn chan net.Conn
 }
 
-func newConnListener(c net.Conn) *connListener {
+// newConnListener returns a net.Listener that will return a single existing connection.
+// Used to bridge an upgraded TLS connection to http.Serve()
+func newConnListener(c net.Conn) net.Listener {
 	ch := make(chan net.Conn, 1)
 	ch <- c
 	return &connListener{conn: ch}
@@ -176,26 +212,3 @@ func (l *connListener) Close() error {
 }
 
 func (l *connListener) Addr() net.Addr { return nil }
-
-func (p *Proxy) signallingHandler(h http.Handler, host string) (http.Handler, <-chan struct{}) {
-	sig := make(chan struct{})
-	return &chanHandlerWrapper{h: h, signal: sig, log: p.log, host: host}, sig
-}
-
-type chanHandlerWrapper struct {
-	signal chan struct{}
-	log    logr.Logger
-	host   string
-	h      http.Handler
-}
-
-func (c *chanHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.URL.Scheme = "https"
-	r.URL.Host = c.host
-	r.Host = c.host
-	c.log.Info("intercepted request", "method", r.Method, "url", r.URL.String())
-	c.h.ServeHTTP(w, r)
-	if w.Header().Get("Connection") == "close" {
-		close(c.signal)
-	}
-}
--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -4,9 +4,11 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thepwagner/hermit/log"
 	"github.com/thepwagner/hermit/proxy"
@@ -19,11 +21,11 @@ var teapot = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 })
 
 func newTestProxy(t *testing.T, opts ...proxy.ProxyOpt) (*proxy.Proxy, *http.Client) {
-	p, err := proxy.NewProxy(nil, append(opts, proxy.ProxyWithLog(log.New()))...)
+	p, err := proxy.NewProxy(teapot, append(opts, proxy.ProxyWithLog(log.New()))...)
 	require.NoError(t, err)
 
 	certs := x509.NewCertPool()
-	// certs.AppendCertsFromPEM(p.Certs.CertPEM())
+	certs.AppendCertsFromPEM(p.IssuerCertPEM())
 	c := &http.Client{
 		Transport: &http.Transport{
 			Proxy:           http.ProxyURL(p.URL()),
@@ -33,45 +35,30 @@ func newTestProxy(t *testing.T, opts ...proxy.ProxyOpt) (*proxy.Proxy, *http.Cli
 	return p, c
 }
 
-// func TestProxy_Http(t *testing.T) {
-// 	p, c := newTestProxy(t, proxy.ProxyWithHost("teapot", teapot))
-// 	defer p.Close()
+func TestProxy_Http(t *testing.T) {
+	p, c := newTestProxy(t)
+	defer p.Close()
 
-// 	t.Run("allowed host", func(t *testing.T) {
-// 		res, err := c.Get("http://teapot")
-// 		require.NoError(t, err)
-// 		defer res.Body.Close()
-// 		require.Equal(t, http.StatusTeapot, res.StatusCode)
+	res, err := c.Get("http://teapot")
+	require.NoError(t, err)
+	defer res.Body.Close()
+	require.Equal(t, http.StatusTeapot, res.StatusCode)
 
-// 		b, err := ioutil.ReadAll(res.Body)
-// 		require.NoError(t, err)
-// 		assert.NotEmpty(t, b)
-// 	})
+	b, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	assert.NotEmpty(t, b)
+}
 
-// 	t.Run("denied host", func(t *testing.T) {
-// 		res, err := c.Get("http://coffeepot")
-// 		require.NoError(t, err)
-// 		defer res.Body.Close()
-// 		require.Equal(t, http.StatusServiceUnavailable, res.StatusCode)
+func TestProxy_Https(t *testing.T) {
+	p, c := newTestProxy(t)
+	defer p.Close()
 
-// 		b, err := ioutil.ReadAll(res.Body)
-// 		require.NoError(t, err)
-// 		assert.NotEmpty(t, b)
-// 	})
-// }
+	res, err := c.Get("https://teapot")
+	require.NoError(t, err)
+	defer res.Body.Close()
+	require.Equal(t, http.StatusTeapot, res.StatusCode)
 
-// func TestProxy_Https(t *testing.T) {
-// 	p, c := newTestProxy(t, proxy.ProxyWithHost("teapot:443", teapot))
-// 	defer p.Close()
-
-// 	t.Run("allowed host", func(t *testing.T) {
-// 		res, err := c.Get("https://teapot")
-// 		require.NoError(t, err)
-// 		defer res.Body.Close()
-// 		require.Equal(t, http.StatusTeapot, res.StatusCode)
-
-// 		b, err := ioutil.ReadAll(res.Body)
-// 		require.NoError(t, err)
-// 		assert.NotEmpty(t, b)
-// 	})
-// }
+	b, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	assert.NotEmpty(t, b)
+}
